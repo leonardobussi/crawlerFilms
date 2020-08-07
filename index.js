cea5ea73650fcbce3a9ba70a2833567af7f9e668
leonardobussi <leonardobezerrabussy@gmail.com>
2020-08-07 02:21:53 -0300

    i

--- index.js
+++ index.js
@@ -1,43 +1,51 @@
 const express  = require('express');
 const request = require('request')
 const cheerio = require('cheerio')
+const bp = require('body-parser');
+
 const fs = require('fs')
 
 const app = express();
+app.use(bp.json());
+app.use(bp.urlencoded({extended: false}));
 
 app.set('view engine', 'ejs');
-
 app.set('views', 'views');
 
-request('http://www.imdb.com/chart/moviemeter', function(err, res, body){
+
+
+request('http://www.imdb.com/chart/moviemeter', function date2 (err, outro, body, dados){
 
     if(err){
-        console.log('Erro:'+err)
+        console.log('Erro: '+err)
     }
 
     var $ = cheerio.load(body)
 
-    $('.lister-list tr').each(function(){
+    $('.lister-list tr').each(function main(){
         const title = $(this).find('.titleColumn a').text().trim()
         const rating = $(this).find('.imdbRating strong').text().trim()
 
-        const dados = {title, rating}
+       const dados = {title, rating}
+       //const content = JSON.stringify(dados);
 
-        fs.appendFile('dados.json', dados ,function(err){
-            if(err){
-               console.error(err)
-            }
-        });
 
-        console.log(dados)
+        //  fs.appendFile('dados.json', `${content},` ,function(err){
+        //         if(err){
+        //            console.error(err)
+        //         }
+        //     });
 
         app.get('/', function(req, res){
-            return res.render('index',{dados: dados}); 
-        })
+            return res.render('index')//,{dados: dados}); 
+         }) 
 
-
+     })
     })
-})
+   
+
+       
+
 
 
 
