2974489df9d108d68f81731090c72a6adef0cc6f
leonardobussi <leonardobezerrabussy@gmail.com>
2020-08-08 16:02:23 -0300

    preciso de ferias em acapuco

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express  = require('express');
-const request = require('request')
-const cheerio = require('cheerio')
 const bp = require('body-parser');
-
-//const fs = require('fs')
+// const fs = require('fs')
+const dadosNow = require('./MineraDados')
 
 const app = express();
+
+
 app.use(bp.json());
 app.use(bp.urlencoded({extended: false}));
 
@@ -13,48 +13,18 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 
-
-request('http://www.imdb.com/chart/moviemeter', function date2 (err, outro, body, dados){
-
-    if(err){
-        console.log('Erro: '+err)
-    }
-
-    var $ = cheerio.load(body)
-
-    $('.lister-list tr').each(function main(){
-        const title = $(this).find('.titleColumn a').text().trim()
-        const rating = $(this).find('.imdbRating strong').text().trim()
-
-       const dados = {title, rating}
-       //const content = JSON.stringify(dados);
-
-
-        //  fs.appendFile('dados.json', `${content},` ,function(err){
-        //         if(err){
-        //            console.error(err)
-        //         }
-        //     });
-
-        app.get('/', function(req, res){
-            return res.render('index')//,{dados: dados}); 
-         }) 
-
-     })
-    })
-   
-
-       
-
-
+app.get('/', function(req, res){
+    return res.send('esta rodando')
+}) 
 
 
 app.listen(3000, (err) => {
     if(err) {
         console.log('==> [-]  falha na aplicação');
+        dadosNow()
     } else {
         console.log('==> [+] aplicação funcionando ');
     }
 });
 
-//module.exports = app;
+module.exports = app;
